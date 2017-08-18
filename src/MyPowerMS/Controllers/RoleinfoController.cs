71574502aa11a9b46709b094e6f7d608e3e7b38f
github <431557313@qq.com>
2017-08-18 21:49:57 +0800

    * 角色管理

--- src/MyPowerMS/Controllers/RoleinfoController.cs
+++ src/MyPowerMS/Controllers/RoleinfoController.cs
@@ -10,7 +10,10 @@ using PagedList;
 
 namespace MyPowerMS.Controllers
 {
-    public class UserinfoController : Controller
+    /// <summary>
+    /// 角色管理
+    /// </summary>
+    public class RoleinfoController : Controller
     {
         UserInfoBLL userbll = new UserInfoBLL();
         RolesInfoBLL rolebll = new RolesInfoBLL();
@@ -19,47 +22,26 @@ namespace MyPowerMS.Controllers
         /// </summary>
         /// <returns></returns>
         public ActionResult Index(int page = 1)
-        {
-            var result = new List<UserViewModel>();
-            var list = userbll.GetAllList().ToList();
-            if (list != null && list.Count > 0)
-            {
-                foreach (var item in list)
-                {
-                    result.Add(new UserViewModel {
-                        Id =item.id,
-                        UserName=item.UserName,
-                        Password=item.PassWord,
-                        RoleName=rolebll.GetById(item.Role)==null ?"": rolebll.GetById(item.Role).RoleName,
-                        TrueName=item.TrueName,
-                        Role=item.Role,
-                        CreateDate=Convert.ToDateTime(item.CreateDate)
-                    });
-                }
-            }
-            return View(result.ToPagedList(page, 2));
+        {            
+            var list = rolebll.GetAllList().ToList();          
+            return View(list.ToPagedList(page, 2));
         }
         public ActionResult Add()
         {
-            ViewBag.RoleList = rolebll.GetAllList();
             return View();
         }
         [HttpPost]
-        public ActionResult Add(string UserName,string TrueName,string Role,string Password)
+        public ActionResult Add(string RoleName, string RoleDesc)
         {
-            T_UserInfo model = new T_UserInfo();
+            T_RoleInfo model = new T_RoleInfo();
             model.id = StringHelper.GetGuid();
-            model.PassWord = Password;
-            model.UserName = UserName;
-            model.TrueName = TrueName;
-            model.CreateDate = DateTime.Now;
-            model.Role = Role;
+            model.RoleName = RoleName;
+            model.RoleDesc = RoleDesc;           
+            model.CreateDate = DateTime.Now;           
             try
             {
-                
-                userbll.Add(model);
-                return Json(new { success = true, msg = "操作成功" });
-          
+                rolebll.Add(model);
+                return Json(new { success = true, msg = "操作成功" });          
             }
             catch
             {
@@ -67,25 +49,33 @@ namespace MyPowerMS.Controllers
             }
 
         }
-
+        /// <summary>
+        /// 编辑
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Edit(string id)
         {
-            ViewBag.RoleList = rolebll.GetAllList();
-            T_UserInfo model = userbll.GetById(id);
+            T_RoleInfo model = rolebll.GetById(id);
             return View(model);
         }
+        /// <summary>
+        /// 更新角色
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="RoleName"></param>
+        /// <param name="RoleDesc"></param>
+        /// <returns></returns>
         [HttpPost]
-        public ActionResult Edit(string id,string UserName, string TrueName, string Role, string Password)
+        public ActionResult Edit(string id, string RoleName, string RoleDesc)
         {
-            T_UserInfo model = userbll.GetById(id);           
-            model.PassWord = Password;
-            model.UserName = UserName;
-            model.TrueName = TrueName;
-            model.CreateDate = DateTime.Now;
-            model.Role = Role;
+            T_RoleInfo model = rolebll.GetById(id);           
+            model.RoleName = RoleName;
+            model.RoleDesc = RoleDesc;          
+            model.CreateDate = DateTime.Now;         
             try
             {
-                if(userbll.Update(model))
+                if(rolebll.Update(model))
                 {
                     return Json(new { success = true, msg = "操作成功" });
                 }
@@ -100,11 +90,11 @@ namespace MyPowerMS.Controllers
             }
         } 
         [HttpPost]
-        public ActionResult DelUser(string id)
+        public ActionResult Del(string id)
         {
             try
             {
-               bool result= userbll.Delete(userbll.GetById(id));
+               bool result= rolebll.Delete(rolebll.GetById(id));
                 if (result)
                 {
                     return Json(new { success = true, msg = "操作成功" });
@@ -121,7 +111,7 @@ namespace MyPowerMS.Controllers
         }
 
         [HttpPost]
-        public ActionResult DelSelectUser(string ids)
+        public ActionResult DelSelect(string ids)
         {
             try
             {
@@ -129,7 +119,7 @@ namespace MyPowerMS.Controllers
                 bool result = false;
                 foreach (var item in arr)
                 {
-                    result = userbll.Delete(userbll.GetById(item));
+                    result = rolebll.Delete(rolebll.GetById(item));
                 }             
                 if (result)
                 {
